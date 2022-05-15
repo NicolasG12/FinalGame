a68a6b6bc84fdffa09a4fa875d681d107b902cd3
NicolasG12 <naguevar@ucsc.edu>
2022-05-15 16:09:59 -0700

    Moved array

--- src/main.js
+++ src/main.js
@@ -29,3 +29,133 @@ let enemyConfig = {
 let keys, cursors;
 
 
+//array for table location
+let labTables = [
+   //top tables
+   {
+       "texture": "labTable1",
+       "x": 400,
+       "y": 15,
+   },
+   {
+       "texture": "labTable2",
+       "x": 255,
+       "y": 15,
+   },
+   {
+       "texture": "labTable2",
+       "x": 125,
+       "y": 15
+   },
+   //middle table area
+   {
+       "texture": "labTable3",
+       "x": 350,
+       "y": 225
+   },
+   {
+       "texture": "labTable4",
+       "x": 300,
+       "y": 175
+   },
+   {
+       "texture": "labTable3",
+       "x": 250,
+       "y": 225
+   },
+   {
+       "texture": "labTable4",
+       "x": 200,
+       "y": 175
+   },
+   {
+       "texture": "labTable3",
+       "x": 150,
+       "y": 225
+   },
+   {
+       "texture": "labTable4",
+       "x": 100,
+       "y": 175
+   },
+   //bottom tables
+   {
+       "texture": "labTable1",
+       "x": 400,
+       "y": 405
+   },
+   {
+       "texture": "labTable2",
+       "x": 255,
+       "y": 405
+   },
+   {
+       "texture": "labTable2",
+       "x": 125,
+       "y": 405
+   },
+];
+
+let hubTables = [
+   {
+       "texture": "table1",
+       "x": 50,
+       "y": 50
+   },
+   {
+       "texture": "table2",
+       "x": 100,
+       "y": 50
+   },
+   {
+       "texture": "table1",
+       "x": 250,
+       "y": 50
+   },
+   {
+       "texture": "table2",
+       "x": 300,
+       "y": 50
+   },
+   {
+       "texture": "table1",
+       "x": 50,
+       "y": 250
+   },
+   {
+       "texture": "table1",
+       "x": 100,
+       "y": 250
+   },
+   {
+       "texture": "table1",
+       "x": 250,
+       "y": 250
+   },
+   {
+       "texture": "table1",
+       "x": 300,
+       "y": 250
+   },
+   
+];
+
+let roomSizes = [
+   {
+       "room": "tutorial",
+       "width": 400,
+       "height": 200
+   },
+   {
+       "room": "hub",
+       "width": 400,
+       "height": 400
+   },
+   {
+       "room": "lab",
+       "width": 500,
+       "height": 500
+   }
+];
+
+
