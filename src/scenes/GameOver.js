199b777b4c4366c9435e85b7a5323ec909cb36f9
NicolasG12 <naguevar@ucsc.edu>
2022-05-15 13:57:38 -0700

    Now able to restart the game

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -3,6 +3,11 @@ class GameOver extends Phaser.Scene {
       super("gameOverScene");
    }
    create() {
+      this.scene.stop("hubScene");
+      this.scene.stop("labScene");
       this.add.text(20, 20, "Game Over");
+      this.input.keyboard.on('keydown-SPACE', () => {
+         this.scene.start("hubScene");
+      });
    }
 }
