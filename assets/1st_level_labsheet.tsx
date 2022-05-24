1c627fd219a1f1f21be2b13bcf089ed1d0206eed
NicolasG12 <naguevar@ucsc.edu>
2022-05-24 12:40:53 -0700

    Removed some unnecessary assets

--- assets/1st_level_labsheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="1st_level_labsheet" tilewidth="32" tileheight="32" tilecount="14" columns="14">
- <image source="1st_level_labsheet.png" width="448" height="32"/>
-</tileset>
--- assets/spritesheet (1).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="spritesheet (1)" tilewidth="32" tileheight="32" tilecount="24" columns="24">
- <image source="spritesheet (1).png" width="768" height="32"/>
-</tileset>
